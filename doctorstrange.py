671690b1ad9aa55ecc52233654cf98660699eb55
datkie <74259871+datkie@users.noreply.github.com>
2020-11-10 08:44:25 -0800

    Add files via upload

---
+++ doctorstrange.py
@@ -0,0 +1,9 @@
+Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:52:53) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:/Users/Comp/AppData/Local/Programs/Python/Python38/1111.py ====
+Math quiz game  Datiles made by Datiles/
+Doctor Strange:Welcome to my game
+Doctor Strange:Beat me by answering  my 11 question correctly !
+It loop when you choose wrong answer
+What is 2+2?:
