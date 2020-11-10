5180bd3b7eef9bec185a144e9638d1443806c9ae
datkie <74259871+datkie@users.noreply.github.com>
2020-11-10 10:12:20 -0800

    Add files via upload

---
+++ doctor strange v2 (addition).py
@@ -0,0 +1,29 @@
+Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:52:53) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:/Users/Comp/AppData/Local/Programs/Python/Python38/1111.py ====
+Math quiz game  Datiles made by Datiles/
+Doctor Strange:Welcome to my game
+Doctor Strange:Beat me by answering  my 30 question correctly !
+It loop when you choose wrong answer
+You have only 15 lives
+What is 164 + 233? 
+===== RESTART: C:/Users/Comp/AppData/Local/Programs/Python/Python38/1111.py ====
+Math quiz game  Datiles made by Datiles/
+Doctor Strange:Welcome to my game
+Doctor Strange:Beat me by answering  my 30 question correctly !
+It loop when you choose wrong answer
+You have only 15 lives
+What is 483 + 381? 864
+Correct!
+You score was : 1
+lives : 15
+Doctor Strange:Next Question?
+What is 180 + 311? 
+===== RESTART: C:/Users/Comp/AppData/Local/Programs/Python/Python38/1111.py ====
+Math quiz game  Datiles made by Datiles/
+Doctor Strange:Welcome to my game
+Doctor Strange:Beat me by answering  my 30 question correctly !
+It loop when you choose wrong answer
+You have only 15 lives
+What is 294 + 163? 
